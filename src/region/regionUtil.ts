44ba33268695aac7192a56017e38e71646e8afe3
Jared Stoffan <jstoffan@box.com>
2020-07-01 11:04:44 -0700

    wip

--- src/region/regionUtil.ts
+++ src/region/regionUtil.ts
@@ -10,6 +10,39 @@ export type Shape = {
 
 export const EMPTY_STYLE = { display: 'none' };
 
+const ROTATION_ONCE_DEG = -90;
+const ROTATION_TWICE_DEG = -180;
+const ROTATION_THRICE_DEG = -270;
+
+export function getRotatedLocation(shape: Shape, rotation: number) {
+    const { height, width, x, y } = shape;
+
+    switch (rotation) {
+        case ROTATION_ONCE_DEG:
+            return [y, height - x];
+        case ROTATION_TWICE_DEG:
+            return [width - x, height - y];
+        case ROTATION_THRICE_DEG:
+            return [width - y, x];
+        default:
+            break;
+    }
+
+    return [x, y];
+}
+
+export const rotateShape = (centerX = 0, centerY = 0, x = 0, y = 0, rotation = 0): Position => {
+    const degrees = rotation >= 0 ? rotation : 360 + rotation; // Convert to positive degrees
+    const radians = (Math.PI / 180) * degrees;
+    const cosine = Math.cos(radians);
+    const sine = Math.sin(radians);
+
+    return {
+        x: (cosine * (x - centerX)) - (sine * (y - centerY)) + centerX, // prettier-ignore
+        y: (cosine * (y - centerY)) + (sine * (x - centerX)) + centerY // prettier-ignore
+    };
+};
+
 export const centerShape = (shape: Shape): Position => {
     const { height, width } = shape;
 
@@ -19,14 +52,13 @@ export const centerShape = (shape: Shape): Position => {
     };
 };
 
-export const centerRegion = (shape: Shape): Position => {
+export const centerRegion = (shape: Shape, rotation = 0): Position => {
     const { x: shapeX, y: shapeY } = shape;
     const { x: centerX, y: centerY } = centerShape(shape);
 
-    return {
-        x: centerX + shapeX,
-        y: centerY + shapeY,
-    };
+    const [x, y] = getRotatedLocation(shape, rotation);
+
+    return { x, y };
 };
 
 export function isRegion(annotation: Annotation): annotation is AnnotationRegion {
--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -2,25 +2,53 @@ import { Position } from '../@types';
 
 export type ScrollOptions = {
     offsets?: Position;
+    rotation?: number;
     threshold?: number;
 };
 
 const DEFAULT_OFFSETS = { x: 0, y: 0 };
+const DEFAULT_ROTATION = 0; // degrees
 const DEFAULT_THRESHOLD = 1000; // pixels
 
+export const rotate = (centerX = 0, centerY = 0, x = 0, y = 0, rotation = 0): Position => {
+    const degrees = rotation >= 0 ? rotation : 360 + rotation; // Convert to positive degrees
+    const radians = (Math.PI / 180) * degrees;
+    const cosine = Math.cos(radians);
+    const sine = Math.sin(radians);
+
+    return {
+        x: (cosine * (x - centerX)) - (sine * (y - centerY)) + centerX, // prettier-ignore
+        y: (cosine * (y - centerY)) + (sine * (x - centerX)) + centerY // prettier-ignore
+    };
+};
+
 export function scrollToLocation(parentEl: HTMLElement, referenceEl: HTMLElement, options: ScrollOptions = {}): void {
-    const { offsets = DEFAULT_OFFSETS, threshold = DEFAULT_THRESHOLD } = options;
+    const { left: parentLeft, top: parentTop } = parentEl.getBoundingClientRect();
+    const {
+        height: referenceHeight,
+        left: referenceLeft,
+        top: referenceTop,
+        width: referenceWidth,
+    } = referenceEl.getBoundingClientRect();
+    const { offsets = DEFAULT_OFFSETS, rotation = DEFAULT_ROTATION, threshold = DEFAULT_THRESHOLD } = options;
     const { x: offsetX, y: offsetY } = offsets;
 
     const canSmoothScroll = 'scrollBehavior' in parentEl.style;
-    const parentCenterX = Math.round(parentEl.clientWidth / 2);
-    const parentCenterY = Math.round(parentEl.clientHeight / 2);
-    const offsetCenterX = Math.round(referenceEl.clientWidth * (offsetX / 100));
-    const offsetCenterY = Math.round(referenceEl.clientHeight * (offsetY / 100));
-    const offsetScrollLeft = referenceEl.offsetLeft - parentCenterX + offsetCenterX;
-    const offsetScrollTop = referenceEl.offsetTop - parentCenterY + offsetCenterY;
-    const scrollLeft = Math.max(0, Math.min(offsetScrollLeft, parentEl.scrollWidth));
-    const scrollTop = Math.max(0, Math.min(offsetScrollTop, parentEl.scrollHeight));
+    const offsetCenterX = Math.round(referenceWidth * (offsetX / 100));
+    const offsetCenterY = Math.round(referenceHeight * (offsetY / 100));
+
+    // const { x, y } = rotate(
+    //     referenceWidth / 2,
+    //     referenceHeight / 2,
+    //     offsetCenterX,
+    //     offsetCenterY,
+    //     rotation,
+    // );
+
+    const offsetLeft = Math.max(0, referenceLeft - parentLeft);
+    const offsetTop = Math.max(0, referenceTop - parentTop);
+    const scrollLeft = Math.max(0, Math.min(offsetLeft + offsetCenterX, referenceWidth));
+    const scrollTop = Math.max(0, Math.min(offsetTop + offsetCenterY, referenceHeight));
 
     if (canSmoothScroll && Math.abs(parentEl.scrollTop - scrollTop) < threshold) {
         parentEl.scrollTo({
